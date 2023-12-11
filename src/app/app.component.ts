d401e7b2a27a21985051f24db25a0ee35a85e73f
Ankit <ankitdhandhalya@gmail.com>
2023-12-11 15:35:03 +0530

    update_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,28 +8,32 @@ import { __values } from 'tslib';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-submitdata() {
-throw new Error('Method not implemented.');
-}
+
   title: any;
   showStorage: any;
-  loginForm: FormGroup | undefined;
+  loginForm: any ;
   fb: any;
-  form: any;
+
 
   constructor() {
-    this.ngOnInit();
+
 }
 
-ngOnInit() {
+ngOnInit(){
     this.loginForm = this.fb.onSubmit({
-        email: ['', [Validators.required, Validators.email],
-        password: ['', [
+        email:['', [Validators.required, Validators.email]],
+        password:['', [
             Validators.required,
             Validators.pattern('((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,30})')
         ]]
     });
 }
+userdetails =[
+  {name : 'naman' , emailid :'naman@test'  },
+  {name : 'aman' , emailid :'aman@test'  },
+  {name : 'om' , emailid :'om@test'  },
+]
+
   // constructor() {
   //   this.loginForm = new FormGroup({
   //     email: new FormControl('', [Validators.required, Validators.email]),
@@ -38,25 +42,21 @@ ngOnInit() {
   // }
 
 
-  onSubmit()
-  users=['naman','aman','om'];
-  userdetails =[
-    {name : 'naman' , emailid :'naman@test'  },
-    {name : 'aman' , emailid :'aman@test'  },
-    {name : 'om' , emailid :'om@test'  },
-  ]
-  {
+  onSubmit(){
+    console.warn(this.loginForm.value);
+}
+  // {
   //   if (this.loginForm.valid) {
   //     console.log(this._form());
   //   }
   // }
   // _form() {
   //   return this.loginForm.value;
-  }
+  // }
 
   submitdata() {
     var value = localStorage.setItem(
-      'submitdata',
+      'userdetails',
       JSON.stringify(this.loginForm.value)
     );
     this.showStorage = localStorage.getItem('submitdata');
